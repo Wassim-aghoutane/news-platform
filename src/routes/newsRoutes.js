a524a281f48208ec1599c4d550df1cf077170560
Wassim-aghoutane <ouasymaghoutane14@gmail.com>
2025-01-02 23:32:43 +0100

    Q4: Compléter les routes CRUD pour la gestion des articles

--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const newsController = require('../controllers/newsController');
 
-// TODO: Question 4 - Compléter les routes
 router.get('/', newsController.getAllNews);
 router.get('/:id', newsController.getNewsById);
 router.post('/', newsController.createNews);
+router.put('/:id', newsController.updateNews);
+router.delete('/:id', newsController.deleteNews);
 
 module.exports = router;
